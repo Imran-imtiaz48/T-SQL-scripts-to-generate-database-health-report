1eb08554c882c9466bcc726eb4eccdec86f732c9
Imran Imtiaz <imranimtiaz48@hotmail.com>
2024-07-22 06:39:18 +0400

    Update SQL Server Health Check Script.sql

--- SQL Server Health Check Script.sql
+++ SQL Server Health Check Script.sql
@@ -1,25 +1,26 @@
 -- Database Information
 SELECT 
-    a.database_id,
-    a.name AS DatabaseName,
-    a.create_date AS CreationDate,
-    b.name AS OwnerName,
-    a.user_access_desc AS UserAccess,
-    a.state_desc AS State,
-    a.compatibility_level AS CompatibilityLevel,
-    a.recovery_model_desc AS RecoveryModel,
-    SUM((c.size * 8) / 1024) AS DBSizeInMB
+    db.database_id,
+    db.name AS DatabaseName,
+    db.create_date AS CreationDate,
+    sp.name AS OwnerName,
+    db.user_access_desc AS UserAccess,
+    db.state_desc AS State,
+    db.compatibility_level AS CompatibilityLevel,
+    db.recovery_model_desc AS RecoveryModel,
+    SUM((mf.size * 8) / 1024) AS DBSizeInMB
 FROM 
-    sys.databases a 
+    sys.databases db
 INNER JOIN 
-    sys.server_principals b ON a.owner_sid = b.sid 
+    sys.server_principals sp ON db.owner_sid = sp.sid
 INNER JOIN 
-    sys.master_files c ON a.database_id = c.database_id
+    sys.master_files mf ON db.database_id = mf.database_id
 WHERE 
-    a.database_id > 5
+    db.database_id > 5
 GROUP BY 
-    a.database_id, a.name, a.create_date, b.name, a.user_access_desc, a.state_desc, 
-    a.compatibility_level, a.recovery_model_desc;
+    db.database_id, db.name, db.create_date, sp.name, 
+    db.user_access_desc, db.state_desc, 
+    db.compatibility_level, db.recovery_model_desc;
 
 -- Server and Instance Status
 DECLARE 
@@ -32,13 +33,13 @@ DECLARE
             WHEN 1 THEN 'Clustered Instance'
             WHEN 0 THEN 'Non-Clustered Instance'
             ELSE 'Unknown'
-         END,
+        END,
     @IsInstanceinSingleUserMode VARCHAR(50) = 
         CASE SERVERPROPERTY('IsSingleUser')
             WHEN 1 THEN 'Single User'
             WHEN 0 THEN 'Multi User'
             ELSE 'Unknown'
-         END;
+        END;
 
 -- Output the Value
 SELECT 
@@ -50,15 +51,15 @@ SELECT
 
 -- Disk Status
 SELECT DISTINCT 
-    volumes.logical_volume_name AS LogicalName,
-    volumes.volume_mount_point AS Drive,
-    CONVERT(INT, volumes.available_bytes / 1024 / 1024 / 1024) AS FreeSpaceGB,
-    CONVERT(INT, volumes.total_bytes / 1024 / 1024 / 1024) AS TotalSpaceGB,
-    CONVERT(INT, (volumes.total_bytes - volumes.available_bytes) / 1024 / 1024 / 1024) AS OccupiedSpaceGB
+    vs.logical_volume_name AS LogicalName,
+    vs.volume_mount_point AS Drive,
+    CONVERT(INT, vs.available_bytes / 1024 / 1024 / 1024) AS FreeSpaceGB,
+    CONVERT(INT, vs.total_bytes / 1024 / 1024 / 1024) AS TotalSpaceGB,
+    CONVERT(INT, (vs.total_bytes - vs.available_bytes) / 1024 / 1024 / 1024) AS OccupiedSpaceGB
 FROM 
     sys.master_files mf
 CROSS APPLY 
-    sys.dm_os_volume_stats(mf.database_id, mf.FILE_ID) volumes;
+    sys.dm_os_volume_stats(mf.database_id, mf.FILE_ID) vs;
 
 -- Database Backup Info
 CREATE TABLE #BackupInformation (
@@ -70,7 +71,7 @@ CREATE TABLE #BackupInformation (
     BackupUser VARCHAR(250)
 );
 
-WITH backup_information AS (
+WITH backup_info AS (
     SELECT
         database_name,
         CASE type
@@ -96,7 +97,7 @@ SELECT
     BackupSizeInMB,
     BackupUser
 FROM 
-    backup_information
+    backup_info
 WHERE 
     rownum = 1
 ORDER BY 
@@ -118,34 +119,34 @@ CREATE TABLE #JobInformation (
 
 INSERT INTO #JobInformation (Servername, CategoryName, JobName, OwnerID, Enabled, NextRunDate, LastRunDate, Status)
 SELECT 
-    CONVERT(VARCHAR, SERVERPROPERTY('Servername')),
-    categories.NAME AS CategoryName,
-    sqljobs.name AS JobName,
-    SUSER_SNAME(sqljobs.owner_sid) AS OwnerID,
-    CASE sqljobs.enabled WHEN 1 THEN 'Yes' ELSE 'No' END AS Enabled,
-    CASE job_schedule.next_run_date
+    CONVERT(VARCHAR, SERVERPROPERTY('Servername')) AS Servername,
+    cat.NAME AS CategoryName,
+    job.name AS JobName,
+    SUSER_SNAME(job.owner_sid) AS OwnerID,
+    CASE job.enabled WHEN 1 THEN 'Yes' ELSE 'No' END AS Enabled,
+    CASE js.next_run_date
         WHEN 0 THEN CONVERT(DATETIME, '1900-01-01')
-        ELSE CONVERT(DATETIME, CONVERT(CHAR(8), job_schedule.next_run_date, 112) 
-            + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), job_schedule.next_run_time), 6), 5, 0, ':'), 3, 0, ':'))
+        ELSE CONVERT(DATETIME, CONVERT(CHAR(8), js.next_run_date, 112) 
+            + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), js.next_run_time), 6), 5, 0, ':'), 3, 0, ':'))
     END AS NextRunDate,
-    lastrunjobhistory.LastRunDate,
-    ISNULL(lastrunjobhistory.run_status_desc, 'Unknown') AS Status
+    ljs.LastRunDate,
+    ISNULL(ljs.run_status_desc, 'Unknown') AS Status
 FROM 
-    msdb.dbo.sysjobs AS sqljobs
+    msdb.dbo.sysjobs AS job
 LEFT JOIN 
-    msdb.dbo.sysjobschedules AS job_schedule ON sqljobs.job_id = job_schedule.job_id
+    msdb.dbo.sysjobschedules AS js ON job.job_id = js.job_id
 LEFT JOIN 
-    msdb.dbo.syscategories AS categories ON sqljobs.category_id = categories.category_id
+    msdb.dbo.syscategories AS cat ON job.category_id = cat.category_id
 LEFT OUTER JOIN (
     SELECT 
-        Jobhistory.job_id,
-        MAX(CASE sysjobhist.run_date
+        jh.job_id,
+        MAX(CASE jh.run_date
             WHEN 0 THEN CONVERT(DATETIME, '1900-01-01')
-            ELSE CONVERT(DATETIME, CONVERT(CHAR(8), sysjobhist.run_date, 112) 
-                + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), sysjobhist.run_time), 6), 5, 0, ':'), 3, 0, ':'))
+            ELSE CONVERT(DATETIME, CONVERT(CHAR(8), jh.run_date, 112) 
+                + ' ' + STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(8), jh.run_time), 6), 5, 0, ':'), 3, 0, ':'))
         END) AS LastRunDate,
-        MAX(sysjobhist.run_status) AS run_status,
-        MAX(CASE sysjobhist.run_status
+        MAX(jh.run_status) AS run_status,
+        MAX(CASE jh.run_status
             WHEN 0 THEN 'Failed'
             WHEN 1 THEN 'Succeeded'
             WHEN 2 THEN 'Retry'
@@ -154,25 +155,38 @@ LEFT OUTER JOIN (
             ELSE 'Unknown'
         END) AS run_status_desc
     FROM 
-        msdb.dbo.sysjobhistory AS sysjobhist
+        msdb.dbo.sysjobhistory AS jh
     WHERE 
-        sysjobhist.step_id = 0  
+        jh.step_id = 0  
     GROUP BY 
-        sysjobhist.job_id
-) AS lastrunjobhistory ON lastrunjobhistory.job_id = sqljobs.job_id;
+        jh.job_id
+) AS ljs ON ljs.job_id = job.job_id;
 
 SELECT * FROM #JobInformation;
 
 -- Monitor and Optimize SQL Database Server
 SELECT 
     @@VERSION AS ServerVersion,
-    STRING_AGG(schema_name, ', ') AS Databases,
-    STRING_AGG(CONCAT(schema_name, ': ', ROUND(SUM((data_length + index_length) / 1024 / 1024), 2), ' MB'), ', ') AS DatabaseSizes,
-    (SELECT ROUND((cntr_value / 1024), 2) FROM sys.dm_os_performance_counters WHERE counter_name = 'Total Server Memory (KB)') AS TotalServerMemory_MB,
-    (SELECT ROUND((cntr_value / 1024), 2) FROM sys.dm_os_performance_counters WHERE counter_name = 'Target Server Memory (KB)') AS TargetServerMemory_MB,
-    (SELECT cntr_value FROM sys.dm_os_performance_counters WHERE counter_name = 'Processor Time' AND object_name = 'Processor' AND instance_name = '_Total') AS CPU_Usage_Percentage,
-    (SELECT STRING_AGG(CONCAT(t1.session_id, ': ', t2.text), '; ') 
-     FROM sys.dm_exec_requests t1 CROSS APPLY sys.dm_exec_sql_text(t1.sql_handle) AS t2 
-     WHERE t1.status = 'running') AS LongRunningQueries
+    STRING_AGG(db.name, ', ') AS Databases,
+    STRING_AGG(CONCAT(db.name, ': ', ROUND(SUM((mf.size * 8) / 1024 / 1024), 2), ' MB'), ', ') AS DatabaseSizes,
+    (SELECT ROUND((pc.cntr_value / 1024), 2) 
+     FROM sys.dm_os_performance_counters pc 
+     WHERE pc.counter_name = 'Total Server Memory (KB)') AS TotalServerMemory_MB,
+    (SELECT ROUND((pc.cntr_value / 1024), 2) 
+     FROM sys.dm_os_performance_counters pc 
+     WHERE pc.counter_name = 'Target Server Memory (KB)') AS TargetServerMemory_MB,
+    (SELECT pc.cntr_value 
+     FROM sys.dm_os_performance_counters pc 
+     WHERE pc.counter_name = 'Processor Time' 
+       AND pc.object_name = 'Processor' 
+       AND pc.instance_name = '_Total') AS CPU_Usage_Percentage,
+    (SELECT STRING_AGG(CONCAT(r.session_id, ': ', t.text), '; ') 
+     FROM sys.dm_exec_requests r 
+     CROSS APPLY sys.dm_exec_sql_text(r.sql_handle) t 
+     WHERE r.status = 'running') AS LongRunningQueries
 FROM 
-    sys.databases;
+    sys.databases db
+JOIN 
+    sys.master_files mf ON db.database_id = mf.database_id
+GROUP BY 
+    db.name;
